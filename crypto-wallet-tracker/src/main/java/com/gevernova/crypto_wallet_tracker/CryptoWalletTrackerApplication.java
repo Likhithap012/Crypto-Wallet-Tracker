24e8a3c5b87d3b63a01d2d15f60348f16070d38b
Likhithap012 <likhithap012@gmail.com>
2025-06-20 10:59:16 +0530

    Added dependencies

---
+++ crypto-wallet-tracker/src/main/java/com/gevernova/crypto_wallet_tracker/CryptoWalletTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gevernova.crypto_wallet_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWalletTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWalletTrackerApplication.class, args);
+	}
+
+}
---
+++ crypto-wallet-tracker/src/test/java/com/gevernova/crypto_wallet_tracker/CryptoWalletTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gevernova.crypto_wallet_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWalletTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
